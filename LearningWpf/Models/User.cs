cca2bd6f9719b1743982e5f26aa7b83e9580b7be
Peter Weinmann <Peter Weinmann@GALADRIEL>
2026-06-27 16:54:13 +0200

    Add project files.

---
+++ LearningWpf/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace LearningWpf.Models
+{
+    internal class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+
+        public List<UserJobGroup> UserJobGroups { get; set; } = new();
+    }
+}
---
+++ LearningWpf/Models/UserJobGroup.cs
@@ -0,0 +1,10 @@
+﻿namespace LearningWpf.Models
+{
+    public class UserJobGroup
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public List<Job> Jobs { get; set; } = new();
+    }
+}
