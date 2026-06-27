fa3f9631687473bc6d86b45661cd41c4c7acea99
Peter Weinmann <sweetpete666@gmail.com>
2026-06-27 17:33:22 +0200

    Kleine Änderungen

--- LearningWpf/Models/JobGroup.cs
+++ LearningWpf/Models/JobGroup.cs
@@ -1,6 +1,6 @@
 ﻿namespace LearningWpf.Models
 {
-    public class UserJobGroup
+    public class JobGroup
     {
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
